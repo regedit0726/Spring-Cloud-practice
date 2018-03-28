38cc078e7add7427ed1056b297ce20e84216f255
regedit0726 <regedit0726@126.com>
2018-03-29 00:04:43 +0800

    init

---
+++ spring-cloud-eurake/src/main/java/com/zzh/springcloudeurake/SpringCloudEurakeApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.springcloudeurake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurakeApplication.class, args);
+	}
+}
